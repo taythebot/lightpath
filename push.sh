4d99ce5dad055d4888c4b88609d2b243fa8ec30f
Defuse Venue <defusevenue@protonmail.com>
2020-08-11 20:19:09 +0900

    Create push.sh

---
+++ push.sh
@@ -0,0 +1,1 @@
+docker build . -t lightpath/openresty:latest && docker tag lightpath/openresty:latest localhost:5000/lightpath/openresty:latest &&  docker push localhost:5000/lightpath/openresty:latest
