f463d63e179dc04360626e014a2c56f050ab0211
Tay <defusevenue@protonmail.com>
2021-10-12 09:38:19 +0900

    Dashboard - Cache settings

--- dashboard/src/views/Zones/Cache.vue
+++ dashboard/src/views/Zones/Cache.vue
@@ -3,10 +3,11 @@
     <div class="col-12 col-md-5">
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Enable Cache</h3>
+          <h3 class="card-title">Purge Edge Cache</h3>
         </div>
         <div class="card-body">
-          <p>Cache responses on the edge</p>
+          <p>Purge specific items from the edge cache by specifying paths</p>
+          <p class="mb-1">Wildcard paths are supported for purging</p>
         </div>
         <div class="card-footer d-flex flex-row-reverse">
           <label class="form-check form-switch m-0 float-end">
@@ -23,13 +24,19 @@
     <div class="col-12 col-md-5">
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Cache TTL</h3>
+          <h3 class="card-title">Edge Cache TTL</h3>
         </div>
         <div class="card-body">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
-            deleniti fugit incidunt, iste, itaque minima neque pariatur
-            perferendis sed suscipit velit vitae voluptatem.
+            The amount of time a cache item should be stored on an edge server
+          </p>
+          <p class="mb-1">
+            <span class="font-weight-medium">Bypass Cache</span> - Bypass edge
+            cache and proxy requests
+          </p>
+          <p class="mt-0">
+            <span class="font-weight-medium">Respect Origin Headers</span> - Use
+            existing cache-control headers
           </p>
         </div>
         <div class="card-footer d-flex flex-row-reverse">
@@ -47,14 +54,14 @@
     <div class="col-12 col-md-5">
       <div class="card">
         <div class="card-header">
-          <h3 class="card-title">Query String</h3>
+          <h3 class="card-title">Sorted Query String</h3>
         </div>
         <div class="card-body">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
-            deleniti fugit incidunt, iste, itaque minima neque pariatur
-            perferendis sed suscipit velit vitae voluptatem.
+            Sort query strings alphabetically, allowing files with different
+            order of query strings to be treated the same
           </p>
+          <p>Enabling this will help increase cache HIT rates</p>
         </div>
         <div class="card-footer d-flex flex-row-reverse">
           <label class="form-check form-switch m-0 float-end">
@@ -74,7 +81,10 @@
           <h3 class="card-title">Strip Cookies</h3>
         </div>
         <div class="card-body">
-          <p>Remove cookies from proxied responses</p>
+          <p>Removes the set-cookie header from origin server</p>
+          <p class="text-warning">
+            Enabling this will break cookie based sessions
+          </p>
         </div>
         <div class="card-footer d-flex flex-row-reverse">
           <label class="form-check form-switch m-0 float-end">
